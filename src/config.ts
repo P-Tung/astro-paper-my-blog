fabc8fe5391190330ae5d5b2b2fe787d26a4600a
TungPhamQ <tungpham2109@gmail.com>
2025-07-04 01:46:21 +0700

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "tommybuilds.site", // replace this with your deployed domain
+  author: "Tommy",
+  profile: "https://tommybuilds.site/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "tommybuilds.site",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
